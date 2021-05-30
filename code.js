307f5c25f3f1756146d6d18c2c01d4561551d2c5
Axil <www.axilje31@gmail.com>
2021-05-30 18:17:18 +0530

    Files added

---
+++ code.js
@@ -0,0 +1,22 @@
+
+function play(e)
+{
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio)return;
+    const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e)
+{
+    if(e.propertyName!='transform')return;
+    this.classList.remove('playing');
+}
+
+const keys=document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+
+window.addEventListener('keydown',play)
+
