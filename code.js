467e7998e91faaf559dada9422f989087f449787
Axil <www.axilje31@gmail.com>
2021-05-31 15:12:02 +0530

    minor changes

--- code.js
+++ code.js
@@ -1,27 +1,18 @@
-
-function play(e)
-{
-    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio)return;
-    const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
-    audio.currentTime=0;
-    audio.play();
-    key.classList.add('playing');
+function play(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
 
-function removeTransition(e)
-{
-    if(e.propertyName!='transform')return;
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName != "transform") return;
+  this.classList.remove("playing");
 }
 
-function help()
-{
-}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-const keys=document.querySelectorAll('.key');
-keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
-
-window.addEventListener('keydown',play)
-
-
+window.addEventListener("keydown", play);
